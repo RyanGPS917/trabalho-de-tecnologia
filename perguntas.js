5fa426e6e1bb0cbdc3dffe9c1d2d6c7a0e6191f4
RyanGPS917 <ryan.salazar@escola.pr.gov.br>
2025-10-30 14:23:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'tecnologia',
+    'Oque significa tecnologia?',
+    'Tecnologia é o conjunto de técnicas, processos, métodos, ferramentas e conhecimentos aplicados para solucionar problemas e atingir objetivos'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
